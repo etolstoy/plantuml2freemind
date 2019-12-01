a7dae20cf82ea3b6f2489acc3aff72b2728264d0
Egor Tolstoy <igrekde@gmail.com>
2019-12-01 14:41:23 +0300

    Added markdown generator

--- plantuml2freemind/custom_types.py
+++ plantuml2freemind/custom_types.py
@@ -2,3 +2,4 @@ from typing import NewType, Dict, Any
 
 # TODO: add real type: attrs is a good choice
 MindmapTreeType = NewType('MindmapTreeType', Dict[str, Any])
+MarkdownTreeType = NewType('MarkdownTreeType', Dict[str, Any])
